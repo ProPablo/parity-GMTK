b31dd287bb8c35df1639b143842ecc1176ee0ac3
Anhad <anhadrs@gmail.com>
2021-06-13 09:52:20 +1000

    Slot changes

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,13 +1,13 @@
 extends Node2D
 export (PackedScene) var Item;
-const Slot = preload("res://scenes/InventorySlot.tscn")
+const Slot = preload("res://scenes/Slot.tscn")
 #Stays constant thanks to settings
 var screen_size;
 export var queue_slots = 6;
-export var total_slots = 1;
-export var min_slot_offset = 0.4;
+export var total_slots = 4;
+export var slots_margin = 0.3;
 
-var inventory = []
+#var inventory = []
 var queue = []
 var slots = []
 
@@ -24,13 +24,15 @@ func _ready():
 func create_inventory():
 	var hud_size = $HUD/InventoryHUD.get_rect().size
 	# if div_x - slot_size - margin/2 is negative, adjust the scale of slot to adjust for that minimum margin  
-	var div_x = hud_size.x /total_slots;
+	var div_margin = hud_size.x * slots_margin; 
+	var div_x = (hud_size.x - div_margin) /total_slots;
 	
 	for i in range(total_slots):
-		inventory.append(null);
+#		inventory.append(null);
 		var slot = Slot.instance();
 		$HUD/InventoryHUD.add_child(slot)
 		slot.name = "InventorySlot%d" % i
+		slot.index = i
 		# var new_pos = Vector2(0, hud_size.y/2)
 		# var slot_sprite = slot.get_child(0);
 		# var div_margin = (div_x - slot_sprite.get_rect().size.x) /2;
@@ -39,7 +41,7 @@ func create_inventory():
 		# 	pass
 		# else:
 		# 	new_pos.x = hud_size.x/2;
-		slot.position = Vector2(i* div_x + 0.5*div_x,hud_size.y/2 )
+		slot.position = Vector2(0.5 * div_margin + i* div_x + 0.5*div_x, hud_size.y/2 )
 		slots.append(slot);
 
 # func create_queue():
@@ -52,8 +54,8 @@ func create_inventory():
 func _on_Item_pickup(item):
 	var is_full = true;
 	for i in range(total_slots):
-		if !inventory[i]:
-			inventory[i] = item
+		if !slots[i].item:
+			slots[i].insert_item(item)
 			item.item_to_inventory(i)
 			is_full = false
 			break
--- scenes/Slot.gd
+++ scenes/Slot.gd
@@ -4,15 +4,31 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var item = null;
+var index = null;
 
+export var shake_scalar = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func insert_item(new_item):
+	item = new_item
+	$SlotAnimation.play("Expiring");
+
+	$SlotAnimation.playback_speed = 2.0;
+
 func _on_slot_expire():
 	print("Fukin done")
+	item.queue_free()
+	item = null
+	$SlotSprite.self_modulate = Color(1.0, 1.0, 1.0, 1.0)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if !item:
+		return
+	$SlotSprite.position = $SlotAnimation.current_animation_position  * shake_scalar
+	pass
